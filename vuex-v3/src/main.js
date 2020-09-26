65ea8eddef42618d63c7afdafa8e9bca7af8f14d
keithmchd48 <keithmchd48@gmail.com>
2020-09-26 13:24:44 +0530

    Vuex-v3 done

--- vuex-v3/src/main.js
+++ vuex-v3/src/main.js
@@ -1,7 +1,35 @@
 import { createApp } from 'vue';
+import {createStore} from "vuex";
 
 import App from './App.vue';
 
 // Add Vuex!
-
-createApp(App).mount('#app');
+const store = createStore({
+  state() {
+    return {
+      users: [],
+    };
+  },
+  mutations: {
+    addUser(state, payload) {
+      const newUser = {
+        id: new Date().toISOString(),
+        name: payload.name,
+      };
+      state.users.push(newUser);
+    },
+  },
+  actions: {
+    addUser(context, payload) {
+      context.commit('addUser', payload);
+    },
+  },
+  getters: {
+    users(state) {
+      return state.users;
+    },
+  },
+})
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
