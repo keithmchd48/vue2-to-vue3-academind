ece19bac03df8f112fc27ccfaea29516c4a278f8
keithmchd48 <keithmchd48@gmail.com>
2020-09-26 12:41:44 +0530

    transitions-v3 done

--- transitions-v3/app.js
+++ transitions-v3/app.js
@@ -1,11 +1,15 @@
-new Vue({
-  el: '#app',
-  data: {
-    isVisible: true
+// basic changes
+const app = Vue.createApp({
+  // basic changes
+  data() {
+    return {isVisible: true}
   },
   methods: {
     toggleBox() {
       this.isVisible = !this.isVisible;
     }
   },
-});
+})
+// basic changes
+app.mount("#app")
+
