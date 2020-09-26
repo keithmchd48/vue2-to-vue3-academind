90321dae27dfbb29b93f6c252c975b0b5065f511
keithmchd48 <keithmchd48@gmail.com>
2020-09-26 13:06:30 +0530

    router-v3 done

--- router-v3/src/main.js
+++ router-v3/src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue';
+import {createRouter, createWebHistory} from "vue-router";
 
 import App from './App.vue';
+import WelcomeScreen from "./pages/WelcomeScreen";
+import UsersList from "./pages/UsersList";
 
 // Add router!
-
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: WelcomeScreen },
+    { path: '/users', component: UsersList },
+  ],
+})
+const app = createApp(App)
+app.use(router)
+// to avoid the initial transition on router-view
+// otherwise just use "app.mount('#app')"
+router.isReady().then(() => {
+  app.mount('#app')
+})
